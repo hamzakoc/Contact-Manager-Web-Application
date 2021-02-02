289acda0587c207a887970c140f59b4734d32fcd
hamzakoc <hamzakooc@gmail.com>
2021-02-02 11:29:15 -0500

    Database Added

---
+++ ContactManagerWebApplication.git/Migrations/20210202154018_Initial.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContactManagerWebApplication.git.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    ContactId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Firstname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Lastname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Organization = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateAdded = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.ContactId);
+                    table.ForeignKey(
+                        name: "FK_Contacts_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Family" },
+                    { 2, "Friend" },
+                    { 3, "Work" },
+                    { 4, "Other" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "ContactId", "CategoryId", "DateAdded", "Email", "Firstname", "Lastname", "Organization", "Phone" },
+                values: new object[] { 1, 1, new DateTime(2021, 2, 2, 10, 40, 18, 296, DateTimeKind.Local).AddTicks(2406), "bruce.wayne@domain.com", "Bruce", "Wayne", null, "416-123-4567" });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "ContactId", "CategoryId", "DateAdded", "Email", "Firstname", "Lastname", "Organization", "Phone" },
+                values: new object[] { 2, 1, new DateTime(2021, 2, 2, 10, 40, 18, 298, DateTimeKind.Local).AddTicks(8819), "bruce.wayne@domain.com", "Parker", "Parker", null, "416-123-4567" });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "ContactId", "CategoryId", "DateAdded", "Email", "Firstname", "Lastname", "Organization", "Phone" },
+                values: new object[] { 3, 1, new DateTime(2021, 2, 2, 10, 40, 18, 298, DateTimeKind.Local).AddTicks(9003), "bruce.wayne@domain.com", "Hamza", "Ha", null, "416-123-4567" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_CategoryId",
+                table: "Contacts",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
---
+++ ContactManagerWebApplication.git/Models/Contact.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ContactManagerWebApplication.git.Models
+{
+    public class Contact
+    {
+        
+        public int ContactId { get; set; }
+        
+        
+        [Required(ErrorMessage ="Please enter a valid First Name")]
+        public string Firstname { get; set; }
+
+        [Required(ErrorMessage = "Please enter a valid Last Name")]
+        public string Lastname { get; set; }
+
+        [Required(ErrorMessage = "Please enter a valid phone")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please enter a valid Email")]
+        public string Email { get; set; }
+
+        public string Organization { get; set; }
+        public DateTime DateAdded { get; set; }
+
+        [Range(1,10000,ErrorMessage = "Please select a category")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public string Slug => Firstname?.Replace(' ', '-').ToLower()
+            +'-' + Lastname.Replace(' ', '-').ToLower();
+
+
+    }
+}
